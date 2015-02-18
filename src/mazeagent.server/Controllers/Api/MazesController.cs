06fd7ff56fe00fb9a89fe5f82a44b0d78ab2fd2c
Michael Levy <michael.levy@sds-consulting.com>
2015-02-17 20:21:52 -0500

    Added start of the webservice

---
+++ src/mazeagent.server/Controllers/Api/MazesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace mazeagent.server.Controllers.Api
+{
+    [RoutePrefix("api/mazes")]
+    public class MazesController : ApiController
+    {
+        /// <summary>
+        /// Return the list of available mazes.
+        /// </summary>
+        /// <example>
+        /// http://example.com/mazes
+        /// </example>
+        [HttpGet, Route("")]
+        public async Task<IHttpActionResult> GetMazes()
+        {
+            return this.InternalServerError();
+        }
+
+        /// <summary>
+        /// Returns information about a specific maze.
+        /// </summary>
+        /// <param name="mId">The m identifier.</param>
+        /// <example>
+        /// http://example.com/mazes/abc
+        /// </example>
+        [HttpGet, Route("{mId:string}")]
+        public async Task<IHttpActionResult> GetMaze(string mId)
+        {
+            return this.InternalServerError();
+        }
+
+        /// <summary>
+        /// Returns information about a specific maze.
+        /// </summary>
+        /// <param name="mId">The m identifier.</param>
+        /// <example>
+        /// http://example.com/mazes/abc/xyz
+        /// </example>
+        [HttpGet, Route("{mId:string}/{cId:string}")]
+        public async Task<IHttpActionResult> NavigateToCell(string mId, string cId)
+        {
+            return this.InternalServerError();
+        }
+
+
+    }
+}
---
+++ src/mazeagent.server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mazeagent.server.WebApiApplication" Language="C#" %>
